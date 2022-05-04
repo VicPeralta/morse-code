68d8f1dcaf1d578575475d65d12f439dce138d6e
JuanLP <juan.luis.palacios.p@gmail.com>
2022-05-04 15:33:59 -0500

    Fix linter errors

--- lib/morse/code/version.rb
+++ lib/morse/code/version.rb
@@ -1,5 +1,5 @@
 module Morse
   module Code
-    VERSION = '0.1.0'
+    VERSION = '0.1.0'.freeze
   end
 end
--- morse.rb
+++ morse.rb
@@ -1,47 +1,48 @@
-MAP = Hash.new
-MAP['.-'] = 'A'
-MAP['-...'] = 'B'
-MAP['-.-.'] = 'C'
-MAP['-..'] = 'D'
-MAP['.'] = 'E'
-MAP['..-.'] = 'F'
-MAP['--.'] = 'G'
-MAP['....'] = 'H'
-MAP['..'] = 'I'
-MAP['.---'] = 'J'
-MAP['-.-'] = 'K'
-MAP['.-..'] = 'L'
-MAP['--'] = 'M'
-MAP['-.'] = 'N'
-MAP['---'] = 'O'
-MAP['.--.'] = 'P'
-MAP['--.-'] = 'Q'
-MAP['.-.'] = 'R'
-MAP['...'] = 'S'
-MAP['-'] = 'T'
-MAP['..-'] = 'U'
-MAP['...-'] = 'V'
-MAP['.--'] = 'W'
-MAP['-..-'] = 'X'
-MAP['-.--'] = 'Y'
-MAP['--..'] = 'Z'
-MAP['-----'] = '0'
-MAP['.----'] = '1'
-MAP['..---'] = '2'
-MAP['...--'] = '3'
-MAP['....-'] = '4'
-MAP['.....'] = '5'
-MAP['-....'] = '6'
-MAP['--...'] = '7'
-MAP['---..'] = '8'
-MAP['----.'] = '9'
+MAP = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
+}.freeze
 
 def decode_char(char)
   MAP[char]
 end
 
 def decode_word(word)
-  characters = word.split(' ')
+  characters = word.split
   decode_word = ''
   characters.each do |c|
     decode_word += decode_char(c)
@@ -52,9 +53,9 @@ end
 def decode(message)
   words = message.split('   ')
   words = words.map { |word| decode_word(word) }
-  return words.join(' ')
+  words.join(' ')
 end
 
 puts 'Enter morse code:'
-input = gets.chomp()
+input = gets.chomp
 puts decode(input)
